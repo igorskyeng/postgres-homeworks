6e422ff4571754c069e4360aa68afd4ce22568cd
Igor Barbur <Igor.barbur@yandex.ru>
2024-02-22 17:11:52 +0800

    Homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customers_id varchar(100) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE employees
+(
+	employees_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	orders_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customers_id) NOT NULL,
+	employee_id int REFERENCES employees(employees_id) NOT NULL,
+	order_date date,
+	ship_city text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,46 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='12345')
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            ways = ['customers_data.csv', 'employees_data.csv', 'orders_data.csv']
+
+            for item in range(0, len(ways)):
+                path = os.path.join(os.path.dirname(__file__), 'north_data/'+ways[item])
+
+                with open(path, 'r', newline='\n', encoding='windows- 1251') as file:
+                    reader = csv.DictReader(file)
+
+                    if ways[item] == 'customers_data.csv':
+                        for line in reader:
+                            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                        (line['customer_id'], line['company_name'], line['contact_name']))
+
+                    elif ways[item] == 'employees_data.csv':
+                        for line in reader:
+                            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                        (line['employee_id'], line['first_name'], line['last_name'],
+                                         line['title'], line['birth_date'], line['notes']))
+
+                    elif ways[item] == 'orders_data.csv':
+                        for line in reader:
+                            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                        (line['order_id'], line['customer_id'], line['employee_id'],
+                                         line['order_date'], line['ship_city']))
+
+            cur.execute("SELECT * FROM customers")
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+except psycopg2.errors.UniqueViolation as error:
+    print(error)
+
+finally:
+    conn.close()
